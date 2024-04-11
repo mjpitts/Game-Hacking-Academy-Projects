b61012ed95ddad8f2449c58e5c3edf8edbfa83f3
mjpitts <mjpitts@ucdavis.edu>
2024-04-11 02:19:36 -0700

    Change structure of the solutions.

--- ExternalMemoryHack/main.cpp
+++ ExternalMemoryHack/main.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 #include <Windows.h>
 
-int main(int argc, char** argv) {
 
+/*
+	Runs external memory hack on "The Battle for Wesnoth 1.14.9.
+
+	External memory hack in general work by using the Windows API to gain a handle over
+	applications. Once the Application can be referenced, the application's memory
+	can be manipulated.
+*/
+void ExternalMemoryHack() {
 
 	// Unsigned 32-bit int,										will store gold memory address.
 	DWORD gold_value = 0;
@@ -20,7 +27,7 @@ int main(int argc, char** argv) {
 	// Get wesnoth process
 	HANDLE wesnoth_process = OpenProcess(PROCESS_ALL_ACCESS, true, process_id);
 
-	
+
 	// Gold Address = [[BS + 0x60] + 0xA90] + 4.
 	// Each bracket indicates a search.
 	ReadProcessMemory(wesnoth_process, (void*)0x017EED18, &gold_value, 4, &bytes_read);
@@ -33,11 +40,30 @@ int main(int argc, char** argv) {
 	gold_value += 4;
 
 	// Gold value in game is set to new_gold_value.
-	DWORD new_gold_value = 444;
+	DWORD new_gold_value = 999;
 	size_t bytes_written;
 
 	// Write new gold value to the game.
 	WriteProcessMemory(wesnoth_process, (void*)gold_value, &new_gold_value, 4, &bytes_written);
+
+}
+
+
+/*
+	Runs injected DLL hack on "The Battle for Wesnoth 1.14.9.
+
+	Injected DLL hack in general work by gaining access to the application's internal memory
+	and using pointers to manipulated that memory. 
+
+*/
+void InternalMemoryHack() {
+
+}
+
+
+int main(int argc, char** argv) {
+
+	ExternalMemoryHack();
 
 	return 0;
 }
